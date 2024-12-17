dfaca21e39b609e94a78836a2d9f1f55a7142c05
Wael <411109515@qu.edu.sa>
2024-12-17 07:54:38 +0300

    Done #2 project

---
+++ Mad Libs Generator.py
@@ -0,0 +1,11 @@
+print("Till Your Story")
+
+name = input("Enter Your Name: ")
+age = int(input("Enter your Age: "))
+hobby= input("Enter your Hooby: ")
+
+till=f"Hi My Name is {name} and I am {age} years old and I want to improve my skills in {hobby}"
+print(till)
+
+
+
